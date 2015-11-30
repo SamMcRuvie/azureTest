22b7eeea1c658eda490de9e70b53a201f98aa061
SamMcRuvie <doctorwhat35@gmail.com>
2015-11-30 15:08:12 +0000

    testing azure MySql
    test

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
+$test = "works";
 
-$test = "hello world";
+$con = mysql_connect('br-cdbr-azure-south-a.cloudapp.net', 'b1d71881ca3bdb', '06bea04b');
+
+if(!$con) die("no conection " . mysql_error());
 
 ?>
-
-
-
 <html>
-	<h1><?php echo($test);  ?></h1>
-
-
+	<h1><?php echo($test); ?></h1>
 </html>
